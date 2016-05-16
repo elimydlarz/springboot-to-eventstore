8aec1b70b5e574046ba585d700fd37820f13ade9
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-05-16 20:10:57 +0800

    Polling for events asynchronously.

--- src/main/java/com/example/HelloController.groovy
+++ src/main/java/com/example/HelloController.groovy
@@ -1,23 +1,39 @@
 package com.example
 
 import com.google.gson.Gson
+import com.mashape.unirest.http.HttpResponse
+import com.mashape.unirest.http.JsonNode
 import com.mashape.unirest.http.Unirest
+import com.mashape.unirest.http.async.Callback
+import com.mashape.unirest.http.exceptions.UnirestException
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
 
 import javax.xml.bind.DatatypeConverter
 import java.security.MessageDigest
-import java.time.LocalTime
-
 
 @RestController
 class HelloController {
 
-  @RequestMapping("/")
+  @RequestMapping('/')
   index() {
-    def eventInput = [currentTime: LocalTime.now().toString()]
-    def eventOutput = Transformer.transform(eventInput)
-    def eventJson = new Gson().toJson([eventOutput])
+     Unirest.get('http://localhost:2113/streams/oldstream/1')
+        .header('Accept', 'application/json')
+        .asJsonAsync(new Callback<JsonNode>() {
+            def failed(UnirestException e) {}
+            def cancelled() {}
+
+            def completed(HttpResponse<JsonNode> response) {
+                def event = Transformer.transform(response.getBody().getArray().get(0))
+                publish(event)
+            }
+        });
+
+    "I'm on it!"
+  }
+
+  private publish(event) {
+    def eventJson = new Gson().toJson([event])
 
     Unirest.post('http://127.0.0.1:2113/streams/newstream')
         .header('content-type', 'application/json')
@@ -25,8 +41,6 @@ class HelloController {
         .header('ES-EventId', hash(eventJson))
         .body(eventJson)
         .asStringAsync()
-
-    "I'm on it!"
   }
 
   private hash(input) {
